3af3f5a89afe79d2befc82952f20d03423b647a8
juliannemarik <julianne.marik@gmail.com>
2023-03-14 15:37:12 -0500

    chore: scaffold project view component

---
+++ src/pages/Project.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        name: "Project",
+        data() {},
+        computed: {},
+        methods: {},
+        mounted() {},
+        beforeUnmount() {},
+    };
+</script>
+
+<template>
+    <MaxWidth class="post" size="m">
+        <h1>PROJECT PAGE</h1>
+    </MaxWidth>
+</template>
+
+<style lang="scss">
+</style>
