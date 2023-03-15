fd9c259ce51d002644c57cd85b2c622fd4c09f34
juliannemarik <julianne.marik@gmail.com>
2023-03-15 17:33:03 -0500

    fix: remove this

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -24,7 +24,7 @@ import { mapState } from 'vuex';
 
 <template>
     <MaxWidth class="post" size="m">
-        <h1>{{this.title}}</h1>
+        <h1>{{title}}</h1>
         <h2>Members</h2>
         <MembersGrid :exclude="nonProjectMembers"/>
         <div class="project-text">
